3d94074a2e20606a672662224e58a6821e11737e
agaevgs_y <agaevgs@yandex-team.ru>
2024-03-04 10:42:30 +0000

    Add range-sum task

--- range-sum/range_sum.cpp
+++ range-sum/range_sum.cpp
@@ -1,10 +1,9 @@
 #include "range_sum.h"
 
 uint64_t RangeSum(uint64_t from, uint64_t to, uint64_t step) {
-    uint64_t result = 0;
-    while (from < to) {
-        result += from;
-        from += step;
+    if (from >= to || step == 0) {
+        return 0;
     }
-    return result;
+    uint64_t n = 1 + ((to - 1) - from) / step;
+    return from * n + step * n * (n - 1) / 2;
 }
