c5ea172bbea1f008cbb1e09850734b19bb6bb3b2
agaevgs_y <agaevgs@yandex-team.ru>
2024-03-10 07:55:17 +0000

    Add semaphore task

--- semaphore/semaphore.h
+++ semaphore/semaphore.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstddef>
 #include <mutex>
 #include <condition_variable>
 
@@ -10,7 +11,9 @@ public:
 
     void Acquire(auto callback) {
         std::unique_lock lock{mutex_};
-        cv_.wait(lock, [this] { return count_; });
+        size_t position(queue_tail_++);
+        cv_.wait(lock, [this, position] { return count_ && queue_head_ == position; });
+        ++queue_head_;
         callback(count_);
     }
 
@@ -21,11 +24,13 @@ public:
     void Release() {
         std::lock_guard lock{mutex_};
         ++count_;
-        cv_.notify_one();
+        cv_.notify_all();
     }
 
 private:
     int count_;
     std::mutex mutex_;
     std::condition_variable cv_;
+    size_t queue_head_ = 0;
+    size_t queue_tail_ = 0;
 };
