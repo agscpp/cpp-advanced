520bb810426245702e2d2c11c91da0cf0870520d
agaevgs_y <agaevgs@yandex-team.ru>
2024-03-10 09:14:36 +0000

    Add rw_lock task

--- rw-lock/rw_lock.h
+++ rw-lock/rw_lock.h
@@ -1,15 +1,14 @@
 #pragma once
 
+#include <condition_variable>
 #include <mutex>
 
 class RWLock {
 public:
     void Read(auto func) {
-        read_.lock();
-        if (++blocked_readers_ == 1) {
-            global_.lock();
-        }
-        read_.unlock();
+        std::unique_lock lock(global_);
+        ++blocked_readers_;
+        lock.unlock();
         try {
             func();
         } catch (...) {
@@ -20,19 +19,21 @@ public:
     }
 
     void Write(auto func) {
-        std::lock_guard lock{global_};
+        std::unique_lock lock{global_};
+        can_write_.wait(lock, [&]() { return blocked_readers_ == 0; });
         func();
+        can_write_.notify_one();
     }
 
 private:
     void EndRead() {
-        std::lock_guard lock{read_};
+        std::unique_lock lock{global_};
         if (--blocked_readers_ == 0) {
-            global_.unlock();
+            can_write_.notify_one();
         }
     }
 
-    std::mutex read_;
+    std::condition_variable can_write_;
     std::mutex global_;
     int blocked_readers_ = 0;
 };
