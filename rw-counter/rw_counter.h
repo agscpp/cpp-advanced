99f4a0002168427e974d5cad53cf62220e8fafba
agaevgs_y <agaevgs@yandex-team.ru>
2024-03-17 12:47:32 +0000

    Add rw_counter task

--- rw-counter/rw_counter.h
+++ rw-counter/rw_counter.h
@@ -1,31 +1,50 @@
 #pragma once
 
+#include <array>
 #include <atomic>
 #include <memory>
+#include <thread>
+
+constexpr std::size_t kHardwareDestructiveInterferenceSize = 64;
+constexpr std::size_t kMaxCounters = 32;
 
 class Incrementer {
 public:
-    explicit Incrementer(std::atomic<int64_t>* counter) : counter_{counter} {
+    explicit Incrementer(std::atomic_int64_t* counter) : counter_(counter) {
     }
 
     void Increment() {
-        ++*counter_;
+        counter_->fetch_add(1, std::memory_order_relaxed);
     }
 
 private:
-    std::atomic<int64_t>* counter_;
+    std::atomic_int64_t* counter_;
 };
 
 class ReadWriteAtomicCounter {
 public:
     std::unique_ptr<Incrementer> GetIncrementer() {
-        return std::make_unique<Incrementer>(&counter_);
+        return std::make_unique<Incrementer>(&counters_[thread_counter_++ % kMaxCounters].value);
     }
 
-    int64_t GetValue() const {
-        return counter_;
+    int64_t GetValue() {
+        while (is_locked_.test_and_set()) {
+            std::this_thread::yield();
+        }
+        int64_t result = 0;
+        for (auto& counter : counters_) {
+            result += counter.value.load();
+        }
+        is_locked_.clear();
+        return result;
     }
 
 private:
-    std::atomic<int64_t> counter_ = 0;
+    struct AlignedInt {
+        alignas(kHardwareDestructiveInterferenceSize) std::atomic_int64_t value;
+    };
+
+    std::atomic_int64_t thread_counter_;
+    std::array<AlignedInt, kMaxCounters> counters_;
+    std::atomic_flag is_locked_;
 };
