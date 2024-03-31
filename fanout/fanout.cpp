6b9e13d6fac6ce82e932fa962ed0f3ef878c5c5d
agaevgs_y <agaevgs@yandex-team.ru>
2024-03-31 21:17:49 +0000

    Add fanout task

--- fanout/fanout.cpp
+++ fanout/fanout.cpp
@@ -1,5 +1,33 @@
 #include "fanout.h"
 
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <vector>
+
 std::string Fanout(const cactus::SocketAddress& address) {
-    return "";
+    auto conn = cactus::DialTCP(address);
+    std::vector<std::unique_ptr<cactus::IConn>> child_conns;
+    size_t number_tokens = 0;
+
+    conn->ReadFull(cactus::View(number_tokens));
+    for (size_t index = 1; index <= number_tokens; ++index) {
+        auto child_conn = cactus::DialTCP({
+            address.GetIp(),
+            static_cast<uint16_t>(address.GetPort() + index),
+        });
+        uint64_t token;
+        conn->ReadFull(cactus::View(token));
+        child_conn->Write(cactus::View(token));
+        child_conns.emplace_back(std::move(child_conn));
+    }
+
+    uint64_t sum = 0;
+    for (const auto& child_conn : child_conns) {
+        uint64_t secret;
+        child_conn->ReadFull(cactus::View(secret));
+        sum += secret;
+    }
+    conn->Write(cactus::View(sum));
+    return conn->ReadAllToString();
 }
--- fanout/main.cpp
+++ fanout/main.cpp
@@ -1,2 +1,29 @@
+#include "fanout.h"
+
+#include <cactus/cactus.h>
+#include <fstream>
+
+constexpr std::chrono::duration kTimeout = std::chrono::seconds(1);
+
+const cactus::SocketAddress kEndpoint{"51.250.102.65", 13000};
+const std::string kFileName = "../fanout/flag.txt";
+
+void WriteTo(const std::string& path, const std::string& data) {
+    std::fstream stream(path, std::fstream::in | std::fstream::out | std::fstream::trunc);
+    if (!stream.is_open()) {
+        throw std::runtime_error("Problems opening file");
+    }
+    stream.write(data.c_str(), data.size());
+    stream.close();
+}
+
 int main() {
+    cactus::Scheduler sched;
+    sched.Run([&] {
+        try {
+            cactus::TimeoutGuard guard{kTimeout};
+            WriteTo(kFileName, Fanout(kEndpoint));
+        } catch (...) {
+        }
+    });
 }
