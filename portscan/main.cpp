f3e8ead764615ac471169d68b327c154002a1835
agaevgs_y <agaevgs@yandex-team.ru>
2024-03-31 12:58:43 +0000

    Add portscan task

--- portscan/main.cpp
+++ portscan/main.cpp
@@ -1,2 +1,24 @@
+#include <cactus/cactus.h>
+
+#include <chrono>
+#include <iostream>
+#include <ostream>
+
+const std::string kHostIp = "51.250.102.65";
+constexpr uint16_t kStartPort = 11'000;
+constexpr uint16_t kEndPort = 11'200;
+constexpr std::chrono::duration kTimeout = std::chrono::seconds(1);
+
 int main() {
+    cactus::Scheduler sched;
+    sched.Run([&] {
+        for (auto port : std::views::iota(kStartPort, kEndPort)) {
+            try {
+                cactus::TimeoutGuard guard{kTimeout};
+                auto conn = cactus::DialTCP({kHostIp, port});
+                std::cout << conn->ReadAllToString() << std::endl;
+            } catch (...) {
+            }
+        }
+    });
 }
--- portscan/portscan.cpp
+++ portscan/portscan.cpp
@@ -1,6 +1,24 @@
 #include "portscan.h"
 
+#include <cactus/cactus.h>
+
+#include <cstddef>
+#include <memory>
+#include <utility>
+
 Ports ScanPorts(const cactus::SocketAddress& remote, uint16_t start, uint16_t end,
                 cactus::Duration timeout) {
-    return {};
+    Ports result;
+    for (auto port : std::views::iota(start, end)) {
+        try {
+            cactus::TimeoutGuard guard{timeout};
+            cactus::DialTCP({remote.GetIp(), port});
+            result.emplace_back(port, PortState::OPEN);
+        } catch (const cactus::TimeoutException& ex) {
+            result.emplace_back(port, PortState::TIMEOUT);
+        } catch (...) {
+            result.emplace_back(port, PortState::CLOSED);
+        }
+    }
+    return result;
 }
